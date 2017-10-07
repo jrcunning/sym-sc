b061bf016e5cbc79c58a9a1450422dc2356636b4
Ross Cunning <ross.cunning@gmail.com>
2017-10-07 09:27:10 -0400

    fix error in counting genes

--- R/filter_sc_orthos.R
+++ R/filter_sc_orthos.R
@@ -6,7 +6,7 @@ library(tidyverse)
 dat <- read.delim("fastOrtho_out/ortho.table",header=F,sep="\t")
 colnames(dat) <- c("Group", "Genes", "Taxa", "IDs")
 dat.all <- filter(dat, Taxa>=4)
-dim(dat.all) #7430 ortholog groups occur in all 3 sym genomes
+dim(dat.all) #6284 ortholog groups occur in all 4 sym genomes
 
 counts <- data.frame(matrix(nrow=nrow(dat.all),ncol=4))
 rownames(counts) <- dat.all$Group
@@ -14,8 +14,8 @@ colnames(counts) <- c("Smic","SymB","Sgor","Skaw")
 
 counts$Smic <- apply(dat.all,1,function(x) str_count(x[4],"Smic.genome.annotation.pep.longest"))
 counts$SymB <- apply(dat.all,1,function(x) str_count(x[4],"symbB.v1.2.augustus.prot"))
-counts$Sgor <- apply(dat.all,1,function(x) str_count(x[4],"SymbC1.Gene_Models.PEP.fasta"))
-counts$Skaw <- apply(dat.all,1,function(x) str_count(x[4],"SymbF.Gene_Models.PEP.fasta"))
+counts$Sgor <- apply(dat.all,1,function(x) str_count(x[4],"SymbC1.Gene_Models.PEP"))
+counts$Skaw <- apply(dat.all,1,function(x) str_count(x[4],"SymbF.Gene_Models.PEP"))
 
 # Find groups with a single occurrence in each genome (purported single copy loci)
 sc <- counts[which(rowSums(counts)==4), ]
