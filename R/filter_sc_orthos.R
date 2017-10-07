76b070e8c3a2292c303397a4d740122898c5f077
Ross Cunning <ross.cunning@gmail.com>
2017-10-07 09:24:41 -0400

    rewrite for 4 genomes

--- R/filter_sc_orthos.R
+++ R/filter_sc_orthos.R
@@ -3,23 +3,24 @@ library(tidyverse)
 
 #args = commandArgs(trailingOnly=TRUE)
 
-dat <- read.delim("protein.ortho.table",header=F,sep="\t")
+dat <- read.delim("fastOrtho_out/ortho.table",header=F,sep="\t")
 colnames(dat) <- c("Group", "Genes", "Taxa", "IDs")
-dat.all <- filter(dat, Taxa>=3)
+dat.all <- filter(dat, Taxa>=4)
 dim(dat.all) #7430 ortholog groups occur in all 3 sym genomes
 
-counts <- data.frame(matrix(nrow=nrow(dat.all),ncol=3))
+counts <- data.frame(matrix(nrow=nrow(dat.all),ncol=4))
 rownames(counts) <- dat.all$Group
-colnames(counts) <- c("Smic","SymB","Skaw")
+colnames(counts) <- c("Smic","SymB","Sgor","Skaw")
 
 counts$Smic <- apply(dat.all,1,function(x) str_count(x[4],"Smic.genome.annotation.pep.longest"))
 counts$SymB <- apply(dat.all,1,function(x) str_count(x[4],"symbB.v1.2.augustus.prot"))
-counts$Skaw <- apply(dat.all,1,function(x) str_count(x[4],"Symbiodinium_kawagutii.0819.final.gene"))
+counts$Sgor <- apply(dat.all,1,function(x) str_count(x[4],"SymbC1.Gene_Models.PEP.fasta"))
+counts$Skaw <- apply(dat.all,1,function(x) str_count(x[4],"SymbF.Gene_Models.PEP.fasta"))
 
 # Find groups with a single occurrence in each genome (purported single copy loci)
-sc <- counts[which(rowSums(counts)==3), ]
+sc <- counts[which(rowSums(counts)==4), ]
 nrow(sc)
 sc.fams <- rownames(sc)
 
-write.table(sc.fams, file="protein.sc.names.txt", row.names=F, col.names=F, quote=F)
+write.table(sc.fams, file="sc_orthos/sc.names.txt", row.names=F, col.names=F, quote=F)
 
