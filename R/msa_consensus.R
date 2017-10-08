401ca2cc1c3e6d897c08d423578fdf9c1136f212
Ross Cunning <ross.cunning@gmail.com>
2017-10-08 11:13:44 -0400

    get each consensus seq

--- R/msa_consensus.R
+++ R/msa_consensus.R
@@ -19,11 +19,17 @@ args = commandArgs(trailingOnly=TRUE)
 fasta <- args[1]
 seqs <- readDNAStringSet(fasta)
 name <- tools::file_path_sans_ext(fasta)
-aln <- msa(seqs, method="Muscle")
-#print(aln, show="complete")
-writeXStringSet(as(aln, "DNAStringSet"), 
-                filepath=paste0(name, ".aln"))
-con <- consensusString(consensusMatrix(aln), threshold=1)
-write(con, file=paste0(name, ".consensus"))
+if (max(width(seqs)) < 10000) {
+  aln <- msa(seqs, method="Muscle")
+  #print(aln, show="complete")
+  writeXStringSet(as(aln, "DNAStringSet"), 
+                  filepath=paste0(name, ".aln"))
+  con <- consensusString(consensusMatrix(aln), threshold=1)
+  write(con, file=paste0(name, ".consensus"))
+} else {
+  write("Error: sequences too long. Alignment in R using Muscle gives fatal error...",
+        file=paste0(name, ".consensus"))
+}
+
 
 
